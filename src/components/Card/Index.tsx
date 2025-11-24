59f2ec624087de2c1a3715162bbdfd436bfa0cf9
Jessicaccl <jessica_cardoso12@yahoo.com.br>
2025-11-24 17:24:57 -0300

    atualizacao

--- src/components/Card/Index.tsx
+++ src/components/Card/Index.tsx
@@ -9,27 +9,27 @@ interface Props {
   item: Pedido;
   onAceitar: () => void;
   esconderBotao?: boolean;
-  openCard: () => void; 
+  openCard: () => void;
 }
 
 export default function CardPedido({ item, onAceitar, esconderBotao, openCard }: Props) {
   return (
     <TouchableOpacity onPress={openCard} style={styles.card}>
-    <View >
-      <Text style={styles.titulo}>{item.title}</Text>
-      
-      <Text style={styles.info}>📅 {new Date(item.data_agendamento).toLocaleString()}</Text>
+      <View >
+        <Text style={styles.titulo}>{item.title}</Text>
 
-      <Text style={styles.info}>📍 {item.location || "Local não informado"}</Text>
+        <Text style={styles.info}>📅 {new Date(item.data_agendamento).toLocaleString()}</Text>
 
-      {!esconderBotao && (
-        <MeuBotao 
-          texto="AGENDAR AJUDA" 
-          cor={colors.success} 
-          onPress={onAceitar} 
-        />
-      )}
-    </View>
+        <Text style={styles.info}>📍 {item.location || "Local não informado"}</Text>
+
+        {!esconderBotao && (
+          <MeuBotao
+            texto="AGENDAR AJUDA"
+            cor={colors.success}
+            onPress={onAceitar}
+          />
+        )}
+      </View>
     </TouchableOpacity>
   );
 }
