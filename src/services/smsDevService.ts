791f8d01b6d410373e4f8aba73d2f3a7f66e6e81
Ireni Iachechen <227895637+Ireni-Iachechen@users.noreply.github.com>
2025-11-26 21:38:53 -0300

    Alteração na posição do botão de alerta.

--- src/services/smsDevService.ts
+++ src/services/smsDevService.ts
@@ -1,9 +1,11 @@
-
 import axios, { AxiosResponse } from 'axios';
 import { BatchMessage, SmsDevBatchResponse, SmsDevResponseItem } from '../@types/smsdev';
 
 const SMSDEV_API_URL = 'https://api.smsdev.com.br/v1/send';
-export const BASE_KEY = 'MH4MFJ2X6U37HF5889C6A24MWHWQE2QSES321NVNSNZNXJZW8DQASQNIZ1CTMTQLN69J31W0XBU8M4I5VM07TEBETFWOHL9FT971KWX6PKIEU5OUNC0F710HL67LM5GQ' ;
+
+// Substitua pela sua chave real da SMSDev
+export const BASE_KEY = 'SUA_CHAVE_DE_API_AQUI';
+
 export const sendBatchAlerts = async (
     alertsToSend: BatchMessage[]
 ): Promise<boolean> => {
@@ -19,9 +21,8 @@ export const sendBatchAlerts = async (
             }
         );
 
-        const responseArray = response.data; 
+        const responseArray = response.data;
 
-      
         const sucessoTotal = responseArray.every(
             (item: SmsDevResponseItem) => item.statusCode === "001" || item.statusCode === "1"
         );
